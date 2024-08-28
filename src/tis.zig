98d5cf0dd0568b2cbf8efdbff64f898346693579
Senryoku <maretverdant@gmail.com>
2024-08-29 01:46:08 +0200

    SequenceCounter

--- src/tis.zig
+++ src/tis.zig
@@ -312,7 +312,9 @@ pub const TIS100 = struct {
         }
 
         // Commit updates
-        // NOTE: This delay all communication between nodes by one cycle. I don't know how to solve this elegantly yet.
+        // NOTE: This delays all communication between nodes by one cycle.
+        //       We could re-run the nodes that stalled because their output wasn't consumed yet,
+        //       but it looks less accurate. Maybe the game does exactly that?
         for (&self.nodes) |*col| {
             for (col) |*node| {
                 for (0..4) |i| {
@@ -344,7 +346,7 @@ pub const TIS100 = struct {
     pub fn print(self: *const @This()) !void {
         inline for (0..4) |i| {
             inline for (0..3) |j| {
-                try Cursor.set(1 + 19 * j, 34 * i);
+                try Cursor.set(1 + 18 * j, 34 * i);
                 try self.nodes[i][j].print();
             }
         }
@@ -865,5 +867,147 @@ test {
 
     for (0..5 * Puzzle.Input0Data.len + 4) |_| {
         tis100.tick();
+    }
+}
+
+const SequenceCounter = struct {
+    const Input0Data = [_]i16{};
+    const Input1Data = [_]i16{ 35, 0, 62, 51, 81, 54, 12, 0, 51, 63, 50, 67, 48, 0, 49, 23, 26, 0, 33, 79, 76, 0, 0, 94, 0, 79, 0, 98, 15, 0, 53, 35, 45, 12, 79, 0, 19, 71, 0 };
+    const Input2Data = [_]i16{};
+    const Input3Data = [_]i16{};
+
+    var input_indices: [4]usize = .{0} ** 4;
+    var output_indices: [4]usize = .{0} ** 4;
+
+    var OutputData: [4][256]i16 = .{.{0} ** 256} ** 4;
+
+    inline fn get_input(idx: u2) []const i16 {
+        return switch (idx) {
+            0 => &Input0Data,
+            1 => &Input1Data,
+            2 => &Input2Data,
+            3 => &Input3Data,
+        };
+    }
+
+    pub fn input_0() ?i16 {
+        defer input_indices[0] += 1;
+        return if (input_indices[0] < get_input(0).len) get_input(0)[input_indices[0]] else null;
+    }
+
+    pub fn input_1() ?i16 {
+        defer input_indices[1] += 1;
+        return if (input_indices[1] < get_input(1).len) get_input(1)[input_indices[1]] else null;
+    }
+
+    pub fn input_2() ?i16 {
+        defer input_indices[2] += 1;
+        return if (input_indices[2] < get_input(2).len) get_input(2)[input_indices[2]] else null;
+    }
+
+    pub fn input_3() ?i16 {
+        defer input_indices[3] += 1;
+        return if (input_indices[3] < get_input(3).len) get_input(3)[input_indices[3]] else null;
+    }
+
+    pub fn output_0(val: i16) void {
+        OutputData[0][output_indices[0]] = val;
+        output_indices[0] += 1;
+    }
+
+    pub fn output_1(val: i16) void {
+        OutputData[1][output_indices[1]] = val;
+        output_indices[1] += 1;
+    }
+
+    pub fn output_2(val: i16) void {
+        OutputData[2][output_indices[2]] = val;
+        output_indices[2] += 1;
+    }
+
+    pub fn output_3(val: i16) void {
+        OutputData[3][output_indices[3]] = val;
+        output_indices[3] += 1;
+    }
+
+    pub fn expected(output: u2, idx: usize) ?i16 {
+        const in1 = [_]i16{ 35, 260, 279, 98, 188, 0, 94, 79, 113, 224, 90 };
+        const in2 = [_]i16{ 1, 5, 5, 3, 3, 0, 1, 1, 2, 5, 2 };
+        switch (output) {
+            0 => return null,
+            1 => return if (idx < in1.len) in1[idx] else null,
+            2 => return if (idx < in2.len) in2[idx] else null,
+            3 => return null,
+        }
+    }
+};
+
+test {
+    var tis100: TIS100 = .{};
+
+    const Puzzle = SequenceCounter;
+    tis100.inputs[0] = &Puzzle.input_0;
+    tis100.inputs[1] = &Puzzle.input_1;
+    tis100.inputs[2] = &Puzzle.input_2;
+    tis100.inputs[3] = &Puzzle.input_3;
+    tis100.outputs[0] = &Puzzle.output_0;
+    tis100.outputs[1] = &Puzzle.output_1;
+    tis100.outputs[2] = &Puzzle.output_2;
+    tis100.outputs[3] = &Puzzle.output_3;
+
+    try tis100.nodes[1][0].set(
+        \\ MOV UP ACC
+        \\ MOV ACC RIGHT
+        \\ MOV ACC DOWN
+    );
+
+    try tis100.nodes[1][1].set(
+        \\ MOV UP ACC
+        \\ JNZ 5
+        \\ MOV 0 DOWN
+        \\ MOV 1 DOWN
+        \\ JMP 0
+        \\ MOV ACC DOWN
+        \\ MOV 3 DOWN
+    );
+
+    try tis100.nodes[1][2].set(
+        \\ ADD UP 
+        \\ JRO UP       
+        \\ MOV ACC DOWN 
+        \\ MOV 0 ACC 
+        \\ NOP 
+    );
+
+    try tis100.nodes[2][0].set(
+        \\ MOV LEFT ACC
+        \\ JNZ 5
+        \\ MOV 0 DOWN
+        \\ MOV 1 DOWN
+        \\ JMP 0
+        \\ MOV 1 DOWN
+        \\ MOV 3 DOWN
+    );
+
+    try tis100.nodes[2][1].set(
+        \\ ADD UP
+        \\ JRO UP
+        \\ MOV ACC DOWN
+        \\ MOV 0 ACC
+        \\ NOP
+    );
+
+    try tis100.nodes[2][2].set(
+        \\ MOV UP DOWN
+    );
+
+    while (Puzzle.expected(0, Puzzle.output_indices[0]) != null and Puzzle.expected(1, Puzzle.output_indices[1]) != null and Puzzle.expected(2, Puzzle.output_indices[2]) != null and Puzzle.expected(3, Puzzle.output_indices[3]) != null) {
+        tis100.tick();
+    }
+
+    for (0..4) |in| {
+        for (0..Puzzle.output_indices[in]) |i| {
+            try std.testing.expect(Puzzle.expected(@intCast(in), i) == Puzzle.OutputData[in][i]);
+        }
     }
 }
